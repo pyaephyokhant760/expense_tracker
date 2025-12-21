df516e27c0514fe49eff74ef6f2d9532944e0f59
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2025-12-21 23:43:05 +0630

    login

--- frontend/my-react-app/src/pages/Auth/Login.jsx
+++ frontend/my-react-app/src/pages/Auth/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import AuthLayout from "../../components/layout/AuthLayout";
+import {useState} from "react-dom";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/input/input";
+
 
 const Login = () => {
-  return (
-    <div>login</div>
-  )
-}
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [error , setError] = useState(null);
 
-export default Login
+  const nagivate = useNavigate();
+
+  const handleLogin = async (e) => {};
+  return (
+    <AuthLayout>
+      <div>
+        <div className="lg:w-[70%] h-3/4 md:h-full flex-col justify-center">
+          <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+          <p className="text-xs text-slate-700 mt-[5px] mb-6">
+            Please Enter Your Details to log in
+          </p>
+
+          <form action="" onSubmit={handleLogin}>
+            <Input value={email} onChange={({target}) => setEmail(target.value)} label="Email Address" placeholder="Admin@gmail.com" type="text"/>
+
+          </form>
+        </div>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
