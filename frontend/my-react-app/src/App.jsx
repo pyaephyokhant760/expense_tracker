db1d608b7ad8c77d2bb56304181364c4df1f821d
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2025-12-21 16:01:12 +0630

    first commit

---
+++ frontend/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div>
+      <h1 className='text-2xl text-red-800'>app</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
