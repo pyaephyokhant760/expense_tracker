7fdaa2fdd8f196d51488f94d5c830ebfc45a3267
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2025-12-23 01:35:41 +0630

    login_page

--- frontend/my-react-app/src/pages/Auth/Sign.jsx
+++ frontend/my-react-app/src/pages/Auth/Sign.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from "react";
+import AuthLayout from "../../components/layout/AuthLayout";
+import {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/input/input";
+import {validateEmail} from "../../utils/helper";
 
 const Sign = () => {
+  const [profilePic,setProfilePic] = useState(null);
+  const [fullname,setFullName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [error,setError] = useState("");
+  const navigete = useNavigate();
   return (
     <div>Sign</div>
   )
